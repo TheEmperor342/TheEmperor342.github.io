572b6d635adb25de1a8039414b010a90caeeead6
TheEmperor342 <83999665+TheEmperor342@users.noreply.github.com>
2022-11-16 18:51:19 +0530

    update

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const ctp = {
   latte: {
     "--bg-0": "#DCE0E8",
     "--bg-1": "#E6E9EF",
+    "--nav-r": 230,
+    "--nav-g": 233,
+    "--nav-b": 239,
     "--bg-2": "#EFF1F5",
     "--txt-color": "#4C4F69",
     "--teal": "#179299",
@@ -12,6 +15,9 @@ const ctp = {
   mocha: {
     "--bg-0": "#161320",
     "--bg-1": "#1A1826",
+    "--nav-r": 24,
+    "--nav-g": 24,
+    "--nav-b": 37,
     "--bg-2": "#1E1E2E",
     "--txt-color": "#D9E0EE",
     "--teal": "#B5E8E0",
@@ -22,6 +28,9 @@ const ctp = {
 let vars = [
   "--bg-0",
   "--bg-1",
+  "--nav-r",
+  "--nav-g",
+  "--nav-b",
   "--bg-2",
   "--txt-color",
   "--teal",

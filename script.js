0cf60fc225a9e0d5545e4b47593f48d834654bd1
TheEmperor342 <83999665+TheEmperor342@users.noreply.github.com>
2022-12-03 11:41:10 +0530

    animations

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const ctp = {
   latte: {
     "--bg-0": "#DCE0E8",
     "--bg-1": "#E6E9EF",
-    "--nav-r": 230,
+    "--overlay-2": "#7c7f93",
+    "--nav-r": 230, 
     "--nav-g": 233,
     "--nav-b": 239,
     "--bg-2": "#EFF1F5",
@@ -15,9 +16,10 @@ const ctp = {
   mocha: {
     "--bg-0": "#161320",
     "--bg-1": "#1A1826",
-    "--nav-r": 24,
-    "--nav-g": 24,
-    "--nav-b": 37,
+    "--overlay-2": "#9399b2",
+    "--nav-r": 17,
+    "--nav-g": 17,
+    "--nav-b": 27,
     "--bg-2": "#1E1E2E",
     "--txt-color": "#D9E0EE",
     "--teal": "#B5E8E0",
@@ -28,6 +30,7 @@ const ctp = {
 let vars = [
   "--bg-0",
   "--bg-1",
+  "--overlay-2",
   "--nav-r",
   "--nav-g",
   "--nav-b",
